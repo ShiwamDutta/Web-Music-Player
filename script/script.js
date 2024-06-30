fab9c3fc26946267954ae00754ca726d4d7ba879
Shiwam Dutta <shiwamdutta3277@gmail.com>
2024-06-30 11:20:04 +0530

    Add play control in song-list and play-bar
    Song to play can be selected from "Your Library" section and play by
    clicking it. They can be paused using paused button in play-bar. If no
    song is selected play button of play-bar will play the first song.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
-async function getSong(folder = "songs") {
-  let localHost = "http://127.0.0.1:3000/Spotify-clone/";
-  let response = await fetch(`${localHost}${folder}/`);
+const songFolder = "/Spotify-clone/songs/";
+const currSong = new Audio();
+
+async function getSong(folder = songFolder) {
+  let localHost = "http://127.0.0.1:3000";
+  let response = await fetch(`${localHost}${folder}`);
   let data = await response.text();
 
   let dummyElement = document.createElement("div");
@@ -10,11 +13,18 @@ async function getSong(folder = "songs") {
   const songs = [];
   Array.from(songLinks).forEach((element) => {
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split(`${folder}/`)[1]);
+      songs.push(element.href.split(`${folder}`)[1]);
     }
   });
 
   return songs;
+}
+
+function playMusic(track) {
+  console.log(songFolder + track);
+  currSong.src = songFolder + track;
+  currSong.play();
+  document.querySelector(".play-bar #play img").src = "./image/pause.svg";
 }
 
 (async function () {
@@ -43,5 +53,29 @@ async function getSong(folder = "songs") {
         alt="Play Now"
       />
     </li>`;
+  });
+
+  // Setting default audio file
+  currSong.src = songFolder + songs[0];
+
+  // Attach event listener to each song
+  Array.from(
+    document.querySelector(".song-list").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", () => {
+      let song = e.querySelector(".song-info .name").innerHTML;
+      playMusic(`${song.replaceAll(" ", "_")}.mp3`);
+    });
+  });
+
+  // Attach event listener to play, skip next & previous buttons of play-bar
+  document.querySelector("#play").addEventListener("click", () => {
+    if (currSong.paused) {
+      currSong.play();
+      document.querySelector("#play img").src = "./image/pause.svg";
+    } else {
+      currSong.pause();
+      document.querySelector("#play img").src = "./image/play.svg";
+    }
   });
 })();
